c6fbb86be39fd83d404ff480d6d1ee6cb7a45dc2
Shivangi <shivangisengar003@gmail.com>
2026-04-01 20:49:01 +0530

    Updated login feature

--- eye humanoid code.cpp
+++ eye humanoid code.cpp
@@ -5,7 +5,7 @@ Servo servoYaw;   // Left/Right movement
 Servo servoPitch; // Up/Down movement
 Servo servoBlink; // Eyelid movement
 
-// ESP32 GPIO Pins (safe PWM pins)
+/ ESP32 GPIO Pins (safe PWM pins)
 const int yawPin = 25;
 const int pitchPin = 26;
 const int blinkPin = 27;
